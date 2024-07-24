bc0a255b4eaa9f00e3c54690c6e0963d0639c82e
pikiminha <106085096+pikiminha@users.noreply.github.com>
2024-07-24 13:19:53 -0300

    vamo ver

--- script.js
+++ script.js
@@ -4,30 +4,79 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
+
 const perguntas = [
     {
-        enunciado: "Voce acha a agricultura importante para a economia? "
+        enunciado: "Você acha agricultura importante para a economia?",
         alternativas: [
             {
-                texto: "Sim!",
-                afirmacao: "A agricultura é importante para a economia pois pela sua exportação muito forte, torna-se uma potencia muito eficiente para a economia."
+                texto: "sim",
+                afirmacao: "a agricultura é importante para a economia por ser uma dos principais formas de sustentaçãodo país com sua exportação de soja e outros plantios"
             },
             {
-                texto: "Não!",
-                afirmacao: "A mudança climatica pode acabar afetano o cultivo."
+                texto: "não",
+                afirmacao: "com a mudança climatica sendo muito forte pode acontecer de se perder todo cultivo e com isso perder os inventimentos na lavoura"
             }
         ]
-},
-{
-    enunciado: "Voce acha a agricultura importante para a economia? "
+    },
+    {
+        enunciado: "A agricultura pode prejudicar a saúde?",
         alternativas: [
             {
-                texto: "Sim!",
-                afirmacao: "A agricultura é importante para a economia pois pela sua exportação muito forte, torna-se uma potencia muito eficiente para a economia."
+                texto: "sim",
+                afirmacao: "realmente, pelo cultivo ter muitos agrotoxicos que podem prejudicar a saúde e meio ambiente, futuramente desencadeando mais problemas"
             },
             {
-                texto: "Não!",
-                afirmacao: "A mudança climatica pode acabar afetano o cultivo."
+                texto: "não",
+                afirmacao:  "existem muitos agricultores familiares que não utilizam dos agrotoxicos ou usam pouco dele"
             }
         ]
-    },        
+    },
+    {
+        enunciado: "A agricultura prejudica o meio ambiente?",
+        alternativas: [
+            {
+                texto: "sim",
+                afirmacao: "com o desmatamento de arvores e gástoxicos que são liberado, com o gasto de petroleo para o combustivel de maquinas e tratores"
+            },
+            {
+                texto: "não",
+                afirmacao: "mesmo com o possivel desmatamento, a agricultura cuida do solo com minerais, sais e ureia, controlando o solo acido e o deixando fertil"
+            }
+        ]
+    },
+];
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+    if (atual >= perguntas.length) {
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    mostraAlternativas();
+}
+
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas) {
+     
+
+
+        function respostaSelecionada(opcaoSelecionada) {
+            const afirmacoes = opcaoSelecionada.afirmacao;
+            historiaFinal += afirmacoes + " ";
+            atual++;
+            mostraPergunta();
+        }
+        
+        function mostraResultado() {
+            caixaPerguntas.textContent = "Em 2049...";
+            textoResultado.textContent = historiaFinal;
+            caixaAlternativas.textContent = "";
+        }
+}
