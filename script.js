640d1c0119de4cb59c8a19c5cc6597fe0db02a10
pikiminha <106085096+pikiminha@users.noreply.github.com>
2024-07-31 13:08:20 -0300

    tudo

--- script.js
+++ script.js
@@ -4,18 +4,17 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-
 const perguntas = [
     {
         enunciado: "Você acha agricultura importante para a economia?",
         alternativas: [
             {
                 texto: "sim",
-                afirmacao: "a agricultura é importante para a economia por ser uma dos principais formas de sustentaçãodo país com sua exportação de soja e outros plantios"
+                afirmacao: "a agricultura é importante para a economia por ser uma dos principais formas de sustentaçãodo país com sua exportação de soja e outros plantios."
             },
             {
                 texto: "não",
-                afirmacao: "com a mudança climatica sendo muito forte pode acontecer de se perder todo cultivo e com isso perder os inventimentos na lavoura"
+                afirmacao: "com a mudança climatica sendo muito forte pode acontecer de se perder todo cultivo e com isso perder os inventimentos na lavoura."
             }
         ]
     },
@@ -24,11 +23,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "sim",
-                afirmacao: "realmente, pelo cultivo ter muitos agrotoxicos que podem prejudicar a saúde e meio ambiente, futuramente desencadeando mais problemas"
+                afirmacao: "realmente, pelo cultivo ter muitos agrotoxicos que podem prejudicar a saúde e meio ambiente, futuramente desencadeando mais problemas."
             },
             {
                 texto: "não",
-                afirmacao:  "existem muitos agricultores familiares que não utilizam dos agrotoxicos ou usam pouco dele"
+                afirmacao:  "existem muitos agricultores familiares que não utilizam dos agrotoxicos ou usam pouco dele."
             }
         ]
     },
@@ -37,15 +36,16 @@ const perguntas = [
         alternativas: [
             {
                 texto: "sim",
-                afirmacao: "com o desmatamento de arvores e gástoxicos que são liberado, com o gasto de petroleo para o combustivel de maquinas e tratores"
+                afirmacao: "com o desmatamento de arvores e gástoxicos que são liberado, com o gasto de petroleo para o combustivel de maquinas e tratores."
             },
             {
                 texto: "não",
-                afirmacao: "mesmo com o possivel desmatamento, a agricultura cuida do solo com minerais, sais e ureia, controlando o solo acido e o deixando fertil"
+                afirmacao: "mesmo com o possivel desmatamento, a agricultura cuida do solo com minerais, sais e ureia, controlando o solo acido e o deixando fertil."
             }
         ]
     },
 ];
+
 
 let atual = 0;
 let perguntaAtual;
@@ -64,19 +64,24 @@ function mostraPergunta() {
 
 function mostraAlternativas(){
     for(const alternativa of perguntaAtual.alternativas) {
-     
-
-
-        function respostaSelecionada(opcaoSelecionada) {
-            const afirmacoes = opcaoSelecionada.afirmacao;
-            historiaFinal += afirmacoes + " ";
-            atual++;
-            mostraPergunta();
-        }
-        
-        function mostraResultado() {
-            caixaPerguntas.textContent = "Em 2049...";
-            textoResultado.textContent = historiaFinal;
-            caixaAlternativas.textContent = "";
-        }
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+    }
 }
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Então...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
